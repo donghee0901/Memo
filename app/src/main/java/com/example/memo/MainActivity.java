ff87de0e697322b47033652b70b433634bd36184
KimDongHee <donghee0901@naver.com>
2020-10-16 21:04:01 +0900

    Test add dynamic Text

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.memo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView test = new TextView(this);
+        ConstraintLayout mainLayout = findViewById(R.id.mainView);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            test.setText("abc");
+            test.setTextSize(100);
+            setContentView(test);
+//            mainLayout.addView(test);
+            test.setText("test");
+        }
+        catch (Exception e)
+        {
+            Log.e("error", e.toString());
+        }
     }
 }
